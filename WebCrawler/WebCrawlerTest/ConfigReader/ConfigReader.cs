139d55ff2468a93504aa959870a7997e9104f1a5
Оксана <oksana.piskunova.ok@gmail.com>
2016-11-08 23:41:56 +0300

    Added reading of the configuration file

---
+++ WebCrawler/WebCrawlerTest/ConfigReader/ConfigReader.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace WebCrawlerTest.ConfigReader
+{
+    internal class ConfigReader : IConfigReader
+    {
+        private const string DepthTag = "depth";
+        private const string RootResourcesTag = "rootResources";
+        private const string ResourceTag = "resource";
+
+        private bool _isFileLoaded;
+        private XElement _rootElement;
+
+        public ConfigReader()
+        {
+            _isFileLoaded = false;
+        }
+        
+        public void LoadFile(string fileName)
+        {
+            if (fileName == null) throw new ArgumentNullException(nameof(fileName));
+
+            var document = XDocument.Load(fileName);
+            _rootElement = document.Root;
+            _isFileLoaded = true;
+        }
+
+        public int GetDepth()
+        {
+            if (!_isFileLoaded) throw new ConfigurationReaderException();
+
+            var depthElement = _rootElement.Element(DepthTag);
+            if (depthElement == null) throw new ConfigurationReaderException();
+            
+            return int.Parse(depthElement.Value);
+        }
+
+        public List<string> GetUrls()
+        {
+            if (!_isFileLoaded) throw new ConfigurationReaderException();
+            
+            var rootResources = _rootElement.Element(RootResourcesTag);
+            if (rootResources == null) throw new ConfigurationReaderException();
+
+            return rootResources.Elements(ResourceTag).Select(resource => resource.Value).ToList();
+        }
+    }
+}
---
+++ WebCrawler/WebCrawlerTest/ConfigReader/ConfigurationReaderException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace WebCrawlerTest.ConfigReader
+{
+    [Serializable]
+    internal class ConfigurationReaderException : Exception
+    {
+        public ConfigurationReaderException()
+        {
+        }
+
+        public ConfigurationReaderException(string message) : base(message)
+        {
+        }
+
+        public ConfigurationReaderException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ConfigurationReaderException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
