506851a79329ebe380519b48e351a4b9f267ee24
dinasergienko <sergidka@gmail.com>
2016-11-11 01:13:22 +0300

    Refactoring

--- WebCrawlerTest/View/MainWindow.xaml.cs
+++ WebCrawlerTest/View/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace WebCrawlerTest
 {
@@ -20,17 +7,9 @@ namespace WebCrawlerTest
     /// </summary>
     public partial class MainWindow : Window
     {
-        private int clickCount = 0;
-
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void ClickButton_Click(object sender, RoutedEventArgs e)
-        {
-           //ClickingInfoLabel.Content = $"You have clicked {++clickCount} times.";
-            
         }
     }
 }
